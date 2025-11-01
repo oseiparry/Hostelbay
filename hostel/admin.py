158dbb80b4e4a9896d1e924f3ec8069a860bbfdf
oseiparry <parryoseistephen@gmail.com>
2025-11-01 11:55:21 +0000

    models were registered in admin site

--- hostel/admin.py
+++ hostel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from.models import User,Hostel,Booking,Report,SystemSetting,Notification    
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Hostel)
+admin.site.register(Booking)
+admin.site.register(SystemSetting)
+admin.site.register(Notification)
+admin.site.register(Report)
